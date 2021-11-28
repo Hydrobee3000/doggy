29702bd1bf9136728b98557f2c52f23a460d6ad4
hydrobee3000 <hydrombee@gmail.com>
2021-11-29 03:30:06 +0600

    added logic for getting data of sub-breeds tostate

--- src/redux/reducers/doggyReducer.js
+++ src/redux/reducers/doggyReducer.js
@@ -2,11 +2,13 @@ import { dogAPI } from './../../components/Api/api'
 
 const SET_DOGGIES = 'SET_DOGGIES' //set data of all breeds
 const SET_BREED = 'SET_BREED' // set data of the selected breed
+const SET_SUB_BREAD = 'SET_SUB_BREAD' // set data of the selected sub-breed
 const SET_BREED_IMAGES = 'SET_BREED_IMAGES' //set URLs data of the selected breed
 
 const initialState = {
   doggiesData: null, //data of all breeds
   dogBreed: 'affenpinscher', //data of the selected breed
+  dogSubBreed: null, //data of the selected sub-breed
   breedImages: null, //array of URLs of the selected breed
 }
 
@@ -24,6 +26,12 @@ const doggyReducer = (state = initialState, action) => {
         dogBreed: action.payload,
       }
     }
+    case SET_SUB_BREAD: {
+      return {
+        ...state,
+        dogSubBreed: action.payload,
+      }
+    }
     case SET_BREED_IMAGES: {
       return {
         ...state,
@@ -38,15 +46,10 @@ const doggyReducer = (state = initialState, action) => {
 
 const setDoggiesData = (doggiesData) => ({ type: SET_DOGGIES, payload: doggiesData })
 export const setBreed = (breed) => ({ type: SET_BREED, payload: breed })
+export const setSubBreed = (dogSubBreed) => ({ type: SET_SUB_BREAD, payload: dogSubBreed }) //установить выбранную подпороду
 const setBreedImages = (breedImages) => ({ type: SET_BREED_IMAGES, payload: breedImages })
 
-// export const toggleIsLoading = (isLoading) => ({
-//   type: TOGGLE_IS_LOADING,
-//   payload: isLoading,
-// })
-
 //fetch data of all breeds and set to state
-
 export const fetchDoggiesData = () => {
   return async (dispatch) => {
     const response = await dogAPI.getAllBreeds()
@@ -58,6 +61,14 @@ export const fetchDoggiesData = () => {
 export const fetchBreedImagesData = (breed) => {
   return async (dispatch) => {
     let response = await dogAPI.getBreedImages(breed)
+    dispatch(setBreedImages(response.data.message))
+  }
+}
+
+//fetch URLs data of the selected sub-breed and set to state
+export const fetchSubBreedImagesData = (breed, subBreed) => {
+  return async (dispatch) => {
+    let response = await dogAPI.getSubBreedImages(breed, subBreed)
     dispatch(setBreedImages(response.data.message))
   }
 }
