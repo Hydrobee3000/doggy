0e3cef19efd10dc3b4673410e9df0e9573ba51e4
hydrobee3000 <hydrombee@gmail.com>
2021-11-29 03:45:56 +0600

    added condition for requesting data of images

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -3,17 +3,21 @@ import { useDispatch, useSelector } from 'react-redux'
 import { fetchBreedImagesData } from '../../redux/reducers/doggyReducer'
 import Preloader from './../common/Preloader'
 import { Main } from './Main/Main'
+import { fetchSubBreedImagesData } from './../../redux/reducers/doggyReducer'
 
 export const MainContainer = () => {
   const dispatch = useDispatch()
 
   const dogBreed = useSelector((state) => state.doggy.dogBreed) //contains data about the selected breed
+  const dogSubBreed = useSelector((state) => state.doggy.dogSubBreed) //contains data about the selected sub-breed
   const breedImages = useSelector((state) => state.doggy.breedImages) //contains data about URL's of the selected breed
 
   useEffect(() => {
-    dispatch(fetchBreedImagesData(dogBreed)) //fetch images of the selected breed
-    return () => {}
-  }, [dispatch, dogBreed])
+    //if there are sub-breeds, then we request images for the sub-breed, else - for the breed
+    dogSubBreed
+      ? dispatch(fetchSubBreedImagesData(dogBreed, dogSubBreed)) //fetch images of the selected sub-breed
+      : dispatch(fetchBreedImagesData(dogBreed)) //fetch images of the selected breed
+  }, [dispatch, dogBreed, dogSubBreed])
 
   //if there are no images in the state, show preloader
   if (!breedImages) {
